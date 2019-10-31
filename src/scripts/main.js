0eba6334cefcc0dc10e0f2230bb66d02b9057cac
James Chapman <chapmanjamesg@gmail.com>
2019-10-31 11:13:15 -0500

    about to start practices

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -112,18 +112,234 @@ const businesses = [
 ];
 
 const outEl = document.querySelector("#output")
-outEl.innerHTML = "<h1>Active Businesses</h1>"
+// outEl.innerHTML = "<h1>Active Businesses</h1>"
+
+// businesses.forEach(business => {
+//     const zipcodeKey = 'addressZipCode'
+//     outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//       ${business.addressCity}, ${business["addressStateCode"]}, ${business[zipcodeKey]}
+//     </section>
+//   `
+//     outEl.innerHTML += "<hr/>"
+// });
+
+//*********************************************************************************************************************************** */
+// Array to contain all the New York businesses
+// const newYorkBusinesses = businesses.filter(business => {
+//     let inNewYork = false
+
+//     if (business.addressStateCode === "NY") {
+//         inNewYork = true
+//     }
+   // console.log('inNewYork', inNewYork)
+//     console.log(business.companyName, inNewYork)
+//     return inNewYork
+// })
+
+// newYorkBusinesses.forEach(business => {
+//     const zipcodeKey = 'addressZipCode'
+//     outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//       ${business.addressCity}, ${business["addressStateCode"]}, ${business[zipcodeKey]}
+//     </section>
+//   `
+//     outEl.innerHTML += "<hr/>"
+// });
+//*********************************************************************************************************************************** */
+// const manufacturingBusinesses = businesses.filter(business => {
+//     if (business.companyIndustry === "Manufacturing") {
+//         return true
+//     }else {
+//         return false
+//     }
+// })
+// manufacturingBusinesses.forEach(business => {
+//     const zipcodeKey = 'addressZipCode'
+//     outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//       ${business.addressCity}, ${business["addressStateCode"]}, ${business[zipcodeKey]}
+//     </section>
+//   `
+//     outEl.innerHTML += "<hr/>"
+// });
+//************************************************************************************************************************************* */
+// outEl.innerHTML += "<h1>Purchasing Agents</h1>";
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+// const agents = businesses.map(business => {
+//     return business
+
+// })
+
+// console.table(agents)
+
+
+// agents.forEach(agent => {
+//   outEl.innerHTML += `<h2>${agent.purchasingAgent.nameFirst} ${agent.purchasingAgent.nameLast}</h2>`;
+//   outEl.innerHTML += `
+//     <section>
+//         ${agent.companyName}
+//     </section>
+//     <section>
+//         ${agent.phoneWork}
+//     </section>
+//     `
+//   outEl.innerHTML += "<hr/>";
+// });
+//**************************************************************************************************************************************** class code */
+
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+// const agents = businesses.map(business => {
+    // return business.purchasingAgent
+//     return {
+//         "fullName": `${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameLast}`,
+//         "company": business.companyName,
+//         'phoneNumber':business.phoneWork
+//     }
+// })
+
+// console.table(agents)
+
+// agents.forEach(agent => {
+//   outEl.innerHTML += `<h2>${agent.fullName}</h2>`;
+//   outEl.innerHTML += `
+//   <section>${agent.company}</section>
+//   <section>${agent.phoneNumber}</section>
+//   `
+//   outEl.innerHTML += "<hr/>";
+// });
+//
+//************************************************************************** */
+
+// const candies = [
+//     {
+//         name: "Lollipop",
+//         price: 2.99
+//     },
+//     {
+//         name: "Tootsie Roll",
+//         price: 1.49
+//     },
+//     {
+//         name: "Sugar Daddy",
+//         price: 2.49
+//     }
+// ]
+
+// const firstCheapCandy = candies.find(candy => candy.price < 2.00)
+
+// console.log(firstCheapCandy)
+// > { name: "Tootsie Roll", price: 1.49 }/
+// document.querySelector('#companySearch').addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+//             const foundBusiness = businesses.find(
+//                 business =>
+//                     business.companyName.includes(keyPressEvent.target.value)
+//             );
+
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundBusiness.companyName}
+//                 </h2>
+//                 <section>
+//                 ${foundBusiness.addressFullStreet}
+
+//                 </section>
+//                 <section>
+//                 ${foundBusiness.addressCity},
+//                 ${foundBusiness.addressStateCode}
+//                 ${foundBusiness.addressZipCode}
+//                 </section>
+//             `;
+//         }
+//     });
+
+
+// outEl.innerHTML += "<h1>Search Results</h1>"
+// document.querySelector("#companySearch")
+//     .addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+            
+//             let foundAgents = businesses.find(business => business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value), business.purchasingAgent.nameLast.includes(keyPressEvent.target.value));
+           
+
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundAgents.purchasingAgent.nameFirst}
+//                 ${foundAgents.purchasingAgent.nameLast}
+//                 </h2>
+//                 <section>
+//                 ${foundAgents.addressFullStreet}
+
+//                 </section>
+//                 <section>
+//                 ${foundAgents.addressCity},
+//                 ${foundAgents.addressStateCode}
+//                 ${foundAgents.addressZipCode}
+//                 </section>
+//             `;
+//         }
+//     });
+//******************************************************************************************************************************* */
+
+// const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+
+// const totalRainfall = monthlyRainfall.reduce((total, currentValue) =>{
+//     console.log(total, currentValue)
+//     return total += currentValue
+//     }) 
+
+// const words = ["The", "quick", "brown", "fox", "jumped", "over", "over", "the", "lazy", "dog"]
+
+// const sentence = words.reduce((sentence, currentWord)=> sentence += ` ${currentWord}`)
+
+// console.log(sentence)
+
+//********************************************************************************************************************************* */
 
 businesses.forEach(business => {
-    const zipcodeKey = 'addressZipCode'
+    /* CALCULATE ORDER SUMMARY */
+    // let totalOrders = 0
+    // business.orders.forEach(order => totalOrders += order)
+
+    let totalOrders = business.orders.reduce((currentTotal, nextValue)=> currentTotal += nextValue)
+
+    
     outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-    <section>
-      ${business.addressCity}, ${business["addressStateCode"]}, ${business[zipcodeKey]}
-    </section>
-  `
-    outEl.innerHTML += "<hr/>"
+
+        <h2>
+            ${business.companyName}
+            ($${totalOrders})
+        </h2>
+        <section>
+            ${business.addressFullStreet}
+        </section>
+        <section>
+            ${business.addressCity},
+            ${business.addressStateCode}
+            ${business.addressZipCode}
+        </section>
+    `;
+    outEl.innerHTML += "<hr/>";
 });
